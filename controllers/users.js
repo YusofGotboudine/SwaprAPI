831e00a2fcec89305a22b7f7f2132cda772f5cd7
YusofGotboudine <yusof95@hotmail.com>
2020-06-11 23:31:50 +0800

    init commit

---
+++ controllers/users.js
@@ -0,0 +1,74 @@
+module.exports = (db) => {
+    const sha256 = require('js-sha256');
+    const spice = "papaya64";
+
+    /**
+     * ===========================================
+     * Controller logic
+     * ===========================================
+     */
+
+    let indexPage = (request, response) => {
+        // response.cookie('user_id', '');
+        // response.cookie('user_id2', '');
+        // response.cookie('logged_in', '');
+        // response.render('users/index');
+        response.send('Hello World');
+    };
+
+    let loginPage = (request, response) => {
+        response.render('users/login');
+    };
+
+    let registrationPage = (request, response) => {
+        response.render('users/register');
+    };
+
+    let postLogin = (request, response) => {
+        console.log('User Logging In');
+
+        db.users.loggedin(request.body, (error, queryResult) => {
+            const data = queryResult.rows[0];
+            console.log(data);
+            let user_id = data.id;
+            let currentSession = sha256(user_id + 'logged' + spice);
+            console.log(currentSession);
+            response.cookie('user_id', user_id);
+            response.cookie('logged_in', currentSession);
+            console.log("Retrieved user data: " + data.name);
+            response.render('users/loggedin', data);
+        });
+    };
+
+    let postRegister = (request, response) => {
+        console.log('Registering User');
+
+        db.users.registering(request.body, (error, queryResult) => {
+            const data = queryResult.rows[0];
+            console.log(data);
+            let user_id = data.id;
+            let currentSession = sha256(user_id + 'logged' + spice);
+            console.log(currentSession);
+            response.cookie('user_id', user_id);
+            response.cookie('logged_in', currentSession);
+            console.log("Registered user data: " + data.name);
+            response.render('users/registered', data);
+        });
+    };
+
+
+    /**
+     * ===========================================
+     * Export controller functions as a module
+     * ===========================================
+     */
+    return {
+        index: indexPage,
+        login: loginPage,
+        register: registrationPage,
+        loggedin: postLogin,
+        registered: postRegister
+
+    };
+
+}
---
+++ routes.js
@@ -0,0 +1,33 @@
+module.exports = (app, allModels) => {
+
+    // require the controller
+    const usersController = require('./controllers/users')(allModels);
+    // const matchesController = require('./controllers/matches')(allModels);
+
+    app.get('/', usersController.index);
+
+    // app.get('/login', usersController.login);
+
+    // app.get('/register', usersController.register);
+
+    // app.post('/loggedin', usersController.loggedin);
+
+    // app.post('/registered', usersController.registered);
+
+    // app.get('/home', matchesController.home);
+
+    // app.get('/profile', matchesController.profile);
+
+    // app.get('/editprofile', matchesController.edit);
+
+    // app.post('/profile/', matchesController.update);
+
+    // app.get('/socialise', matchesController.socialise);
+
+    // app.get('/socialising', matchesController.socialising);
+
+    // app.get('/profile/:id', matchesController.viewsingleprofile );
+    // match conditional
+
+
+};
